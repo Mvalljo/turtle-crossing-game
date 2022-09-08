6894e99f5003e16cb95c6c4074831b7bd0da0985
maria vallejo <maria.vallejo29@yahoo.com>
2022-09-07 19:52:56 -0500

    add fcn for turtle at finish line in player file

--- turtle-crossing-start/player.py
+++ turtle-crossing-start/player.py
@@ -19,5 +19,8 @@ class Player(Turtle):
     def starting_position(self):
         self.goto(STARTING_POSITION)
 
-
-
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
