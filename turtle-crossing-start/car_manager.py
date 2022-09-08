6011cde4bfc4a59380783d99c75854f0403f4f7c
maria vallejo <maria.vallejo29@yahoo.com>
2022-09-07 19:40:11 -0500

    redo moving cars fcn and make a level up fcn

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -10,7 +10,7 @@ class CarManager(Turtle):
     def __init__(self):
         super().__init__()
         self.all_cars = []
-        self.move_speed = 0.1
+        self.car_speed = 0.1
 
     def create_car(self):
         random_chance = random.randint(1, 6)
@@ -23,6 +23,10 @@ class CarManager(Turtle):
             new_car.goto(300, random_y)
             self.all_cars.append(new_car)
 
-    def moving_car(self):
-        self.forward(MOVE_INCREMENT)
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
 
