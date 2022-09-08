49d6626cd51e65b9fcb137b85958806ee5c1aa41
maria vallejo <maria.vallejo29@yahoo.com>
2022-09-07 20:07:05 -0500

    redo car manager in main file

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -10,6 +10,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 scoreboard = Scoreboard()
 
 screen.listen()
@@ -20,13 +21,19 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    car_manager.create_car()
+    car_manager.move_cars()
+
     # turtle crosses finish line
-    if player.ycor() > 280:
+    if player.is_at_finish_line():
         player.starting_position()
+        car_manager.level_up()
         scoreboard.increase_score()
 
-    # Every 6th loop make a new car
-
-    # Move forward cars in list
+    # turtle crashes into car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
